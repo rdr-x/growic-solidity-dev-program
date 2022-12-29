56e70f62672e775730682f2ffe9e8f959bbd966d
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2022-12-29 10:06:07 -0600

    network config

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ```shell
 npx hardhat help
 npx hardhat test
-REPORT_GAS=true npx hardhat test
+REPORT_GAS=true
+npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+  goerli: {
+    url: process.env.TEST_URL,
+    accounts: [`${process.env.TEST_PVK}`]
+    }
+  }
 };
 
 export default config;
