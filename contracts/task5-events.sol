e6ad13cd7fa14a4cecc9adcba21073b9a268030f
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2022-12-29 14:48:46 -0600

    [ADD] task5 events uploaded | const from task 4 updated

--- contracts/task5-events.sol
+++ contracts/task5-events.sol
@@ -2,24 +2,30 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 contract StructsTaskContract {
-
+    //variables and constants
     address public owner;
     uint256 private constant Fee = 3;
-
+    //constructor called just when the contract is created/deployed
     constructor () {
         owner = msg.sender;
     }
-
+    //struct data types
     struct User {
         string name;
         uint256 age;
         uint256 amount;
     }
-
+    //mapping 
     mapping (address => User) public user;
 
+    //The contact should emit the following events when a user deposits and updates their profile information respectively:
+    event FundsDeposited(address _user, uint256 _amount);
+    event ProfileUpdated(address _user);
+
+    //functions
     function deposit(uint256 _amount) public {
         user[msg.sender].amount += _amount;
+        emit FundsDeposited(msg.sender, _amount);
     }
 
     function checkBalance() public view returns(uint256) {
@@ -29,6 +35,7 @@ contract StructsTaskContract {
     function setUserDetails(string calldata _name, uint256 _age) public {
         user[msg.sender].name = _name;
         user[msg.sender].age = _age;
+        emit ProfileUpdated(msg.sender);
     }
 
     function getUserDetail() public view returns(User memory) {
