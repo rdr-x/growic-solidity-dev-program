de3a17ba03ba1ea0b2cdd461d068eadb6724a78c
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2022-12-30 10:30:55 -0600

    [ADD] task6 contract added and deploy on goerli. SC Address:  0x55F4e1A6a52051cdbAf39EafAE14e032502550E5

--- contracts/task6-inheritance.sol
+++ contracts/task6-inheritance.sol
@@ -1,7 +1,15 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-contract InheritenceTaskContract {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract InheritenceTaskContract is ERC20, Ownable {
+    constructor() ERC20("InheritenceTaskToken", "ITT") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
 
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
