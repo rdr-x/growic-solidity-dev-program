887044cb0b397c07f0bd755fac20b94fe0a6ad5c
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2023-01-19 21:25:26 -0600

    [ADD] constructor task contract

---
+++ contracts/task9-constructor.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+contract ConstructorTaskContractA {
+    address immutable private owner;
+    uint FEE;
+
+    constructor (uint _fee) {
+            owner = msg.sender;
+            FEE = _fee;
+    }
+}
+
+contract ConstructorTaskContractB is ConstructorTaskContractA {
+    
+    address owner;
+
+    constructor () ConstructorTaskContractA(20) {
+        
+    }
+}
